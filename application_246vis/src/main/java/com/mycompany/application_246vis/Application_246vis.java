e5c4f8bb30d00684c3d3ce5421fb7ced574bf483
DUY NGUYEN <DUY NGUYEN@172.25.15.138>
2025-03-02 08:17:37 +0700

    fist

---
+++ application_246vis/src/main/java/com/mycompany/application_246vis/Application_246vis.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.application_246vis;
+
+/**
+ *
+ * @author DUY NGUYEN
+ */
+public class Application_246vis {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
